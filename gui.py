9440f74e3ebf3b3f3d9bd0bf8f3e1d579d227a16
Nguyễn Thế Phong <nguyenthephong508@gmail.com>
2023-09-02 03:03:43 +0700

    snapshot function completed

--- gui.py
+++ gui.py
@@ -1,22 +1,152 @@
 import sys
+from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QDialog, QMainWindow, QPushButton
+from myOCR_prototype.takeScrShot import SnippingWidget
+from PyQt6.QtCore import *
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
+class Ui_Mainwindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(932, 667)
+        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.widget = QtWidgets.QWidget(parent=self.centralwidget)
+        self.widget.setGeometry(QtCore.QRect(310, 50, 311, 26))
+        self.widget.setObjectName("widget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pushButton_3 = QtWidgets.QPushButton(parent=self.widget)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.horizontalLayout.addWidget(self.pushButton_3)
+        self.pushButton_2 = QtWidgets.QPushButton(parent=self.widget)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.pushButton = QtWidgets.QPushButton(parent=self.widget)
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
+        self.widget1 = QtWidgets.QWidget(parent=self.centralwidget)
+        self.widget1.setGeometry(QtCore.QRect(20, 100, 881, 491))
+        self.widget1.setObjectName("widget1")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.widget1)
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.graphicsView = QtWidgets.QLabel(parent=self.widget1)
+        self.graphicsView.setObjectName("graphicsView")
+        self.horizontalLayout_2.addWidget(self.graphicsView)
+        self.textBrowser = QtWidgets.QTextBrowser(parent=self.widget1)
+        self.textBrowser.setObjectName("textBrowser")
+        self.horizontalLayout_2.addWidget(self.textBrowser)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(parent=MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 932, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
-from PyQt6.QtWidgets import (
-    QApplication,
-    QPushButton,
-    QVBoxLayout,
-    QWidget,
-)
+        self.graphicsView.setMinimumSize(QSize(800, 800))
+        self.graphicsView.setMaximumSize(QSize(4096, 4096))
 
-class MainWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("QVBoxLayout Example")
-        self.resize(270, 110)
-        # Create a QVBoxLayout instance
-        layout = QVBoxLayout()
-        # Add widgets to the layout
-        layout.addWidget(QPushButton("Top"))
-        layout.addWidget(QPushButton("Center"))
-        layout.addWidget(QPushButton("Bottom"))
-        # Set the layout on the application's window
-        self.setLayout(layout)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton_3.setText(_translate("MainWindow", "Start"))
+        self.pushButton_2.setText(_translate("MainWindow", "PushButton"))
+        self.pushButton.setText(_translate("MainWindow", "PushButton"))
+
+
+
+
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.ui = Ui_Mainwindow()
+        self.ui.setupUi(self)
+
+        self.ui.pushButton.clicked.connect(self.button1Clicked)
+        self.ui.pushButton_3.clicked.connect(self.button3Clicked)
+
+        self.snippingWidget = SnippingWidget(app=QApplication.instance())
+        self.snippingWidget.onSnippingCompleted = self.onSnippingCompleted
+
+        self._pixmap = None
+
+
+    def resizeImage(self, pixmap):
+        lwidth = self.ui.graphicsView.width()
+        pwidth = pixmap.width()
+        lheight = self.ui.graphicsView.height()
+        pheight = pixmap.height()
+
+        wratio = pwidth * 1.0 / lwidth
+        hratio = pheight * 1.0 / lheight
+
+        if pwidth > lwidth or pheight > lheight:
+            if wratio > hratio:
+                lheight = pheight / wratio
+            else:
+                lwidth = pwidth / hratio
+
+            scaled_pixmap = pixmap.scaled(lwidth, lheight)
+            return scaled_pixmap
+        else:
+            return pixmap
+
+    def onSnippingCompleted(self,frame):
+        self.setWindowState(QtCore.Qt.WindowState.WindowActive)
+        if frame is None:
+            return
+
+        image = QImage(frame, frame.shape[1], frame.shape[0], frame.strides[0], QImage.Format.Format_RGB888)
+        image.save("snapshot.png")
+        pixmap = QPixmap.fromImage(image)
+        self._pixmap = self.resizeImage(pixmap)
+        self.ui.graphicsView.setPixmap(self._pixmap)
+    def button1Clicked(self):
+        dlg = Dialog(self)
+        dlg.exec()
+    def button3Clicked(self):
+        self.setWindowState(QtCore.Qt.WindowState.WindowMinimized)
+        self.snippingWidget.start()
+
+
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(627, 400)
+        self.pushButton = QtWidgets.QPushButton(parent=Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(270, 160, 80, 24))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.pushButtonClicked)
+
+        self.variable = 0
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.pushButton.setText(_translate("Dialog", "OK"))
+
+    def pushButtonClicked(self):
+        print(self.variable)
+        self.variable += 1
+
+class Dialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        # Create an instance of the GUI
+        self.ui = Ui_Dialog()
+        # Run the .setupUi() method to show the GUI
+        self.ui.setupUi(self)
